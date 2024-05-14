7834ee496df1ecd1d5da4eea901a4ed28e938dfd
Gabriel Barbosa <gabrielbarbos210@gmail.com>
2024-05-14 16:59:45 -0300

    Initial Commit

---
+++ BuscarCep/BuscarCepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuscarCepApp.swift
+//  BuscarCep
+//
+//  Created by Gabriel Barbosa on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BuscarCepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
